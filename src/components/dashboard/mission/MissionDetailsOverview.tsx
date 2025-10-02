04ec2cd18f602e4aa5162f27fe5181f088e7c4e2
01JAMIL <jamilbenbrahim1@gmail.com>
2025-10-02 20:23:19 +0100

    feat: Refactor MissionDetailsOverview and simplify MissionDetailsPageData for improved clarity and structure

--- src/components/dashboard/mission/MissionDetailsOverview.tsx
+++ src/components/dashboard/mission/MissionDetailsOverview.tsx
@@ -22,17 +22,65 @@ const MissionDetailsOverview = ({mission}: MissionDetailsOverviewProps) => {
                 </p>
             </div>
 
-            {/*<div className="flex flex-col gap-3">
-                <p className="text-[#101828] font-semibold text-lg">
-                    Détails de la mission
+            <div className="w-full h-[1px] bg-[#EAECF0]"/>
+
+            <p className="font-medium tracking-tighter text-3xl text-[#101828] bricolage-grotesque">
+                Profil recherché
+            </p>
+            <div className="w-full h-[1px] bg-[#EAECF0]"/>
+
+            <div className="flex flex-col gap-3">
+                <p className="font-medium text-[#101828] tracking-tighter bricolage-grotesque">
+                    Domaine d’invervention
                 </p>
-                <p className="text-[#475467]">
-                    Dolor enim eu tortor urna sed duis nulla. Aliquam vestibulum, nulla odio nisl vitae. In aliquet
-                    pellentesque aenean hac vestibulum turpis mi bibendum diam. Tempor integer aliquam in vitae
-                    malesuada fringilla. Elit nisi in eleifend sed nisi. Pulvinar at orci, proin imperdiet commodo
-                    consectetur convallis risus.
+                <div className="flex flex-wrap gap-3">
+                    {
+                        mission.industry.map((industry, index) => (
+                            <div
+                                key={index}
+                                className="rounded-[0.375rem] px-2 py-0.75 border border-[#D0D5DD] bg-white text-xs text-[#344054]">
+                                {industry}
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-3">
+                <p className="font-medium text-[#101828] tracking-tighter bricolage-grotesque">
+                    Compétences
                 </p>
-            </div>*/}
+                <div className="flex flex-wrap gap-3">
+                    {
+                        mission.skills.map((skill, index) => (
+                            <div
+                                key={index}
+                                className="rounded-[0.375rem] px-2 py-0.75 border border-[#D0D5DD] bg-white text-xs text-[#344054]">
+                                {skill}
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className="w-full h-[1px] bg-[#EAECF0]"/>
+
+            <p className="font-medium tracking-tighter text-3xl text-[#101828] bricolage-grotesque">
+                Informations sur le client
+            </p>
+            <div className="w-full h-[1px] bg-[#EAECF0]"/>
+
+            <div className="flex flex-col gap-3">
+                <p className="font-medium text-[#101828] tracking-tighter bricolage-grotesque">
+                    Nom de l’entreprise
+                </p>
+                <div className="flex flex-wrap gap-3">
+                    <div
+                        className="rounded-[0.375rem] px-2 py-0.75 border border-[#D0D5DD] bg-white text-xs text-[#344054]">
+                        {mission.client}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/dashboard/mission/MissionDetailsPageData.tsx
+++ src/components/dashboard/mission/MissionDetailsPageData.tsx
@@ -1,10 +1,5 @@
-import React, {useState} from 'react';
-import {useSearchParams} from "next/navigation";
-import {MissionTabsKey} from "@/types/mission/MissionTabsKey";
-import MissionNavigationTabs from "@/components/dashboard/mission/MissionNavigationTabs";
+import React from 'react';
 import MissionDetailsOverview from "@/components/dashboard/mission/MissionDetailsOverview";
-import MissionDetailsProfile from "@/components/dashboard/mission/MissionDetailsProfile";
-import MissionDetailsClient from "@/components/dashboard/mission/MissionDetailsClient";
 import {MissionDetails} from "@/types/mission/MissionDetails";
 
 interface MissionDetailsPageDataProps {
@@ -13,26 +8,11 @@ interface MissionDetailsPageDataProps {
 
 const MissionDetailsPageData = ({mission}: MissionDetailsPageDataProps) => {
 
-    const searchParams = useSearchParams();
-    const tabParam = searchParams.get("tab") as MissionTabsKey;
-    const [currentTab, setCurrentTab] = useState<MissionTabsKey>(tabParam || "overview");
 
     return (
         <div className="flex flex-col gap-8 w-full h-full">
-            <MissionNavigationTabs
-                onTabChange={(tab) => setCurrentTab(tab)}
-            />
-
             <div className="h-full">
-                {
-                    currentTab === "overview" ? <MissionDetailsOverview mission={mission}/> : null
-                }
-                {
-                    currentTab === "profile" ? <MissionDetailsProfile mission={mission}/> : null
-                }
-                {
-                    currentTab === "client" ? <MissionDetailsClient mission={mission}/> : null
-                }
+                <MissionDetailsOverview mission={mission}/>
             </div>
 
         </div>
